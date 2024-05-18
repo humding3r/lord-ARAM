f2d63e1180af5415aacd8dcacf813affbb1f3206
humding3r <jetgeronimo@gmail.com>
2024-05-18 17:06:16 -0500

    Restructure project files Create src directory Add relevant entries to .gitignore

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { token } = require("./config.json");
+const { token } = require("../config.json");
 const { Client, Events, GatewayIntentBits, SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const axios = require('axios');
 const cheerio = require('cheerio');
